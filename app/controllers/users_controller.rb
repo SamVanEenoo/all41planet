8cb616f0a333c2ec5dc1c6228671cd9d3883b80d
Sam Van Eenoo <sam@getsilverfin.com>
2022-05-31 15:21:46 +0200

    updated user controller for registration and layout update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    throw :foo
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new, notice: "Something went wrong"
+    end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:password,:password_confirmation,:avatar)
+  end
 end
