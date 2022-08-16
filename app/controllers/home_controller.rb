351d1fe7f73c5769356725f0b4bba3b26524f8ef
Sam Van Eenoo <sam@getsilverfin.com>
2022-08-16 11:10:32 +0200

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,7 @@ class HomeController < ApplicationController
     if current_user
       redirect_to articles_path
     else
-      @users = User.all
-      @last_5_users = @users.last(5)
-      @company_users = CompanyUser.all
-      @last_5_company_users = @company_users.last(5)
-      @projects = Project.all
-      @last_5_projects = @projects.last(5)
-      render :lists
+      render :index
     end
   end
 
