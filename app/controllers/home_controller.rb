12a7a61a92deb7239cc846269fd6e40a29f96f7e
Sam Van Eenoo <sam@getsilverfin.com>
2022-08-23 21:31:33 +0200

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    if current_user
-      redirect_to articles_path
-    else
-      render :index
-    end
+    render :index
   end
 
   def lists
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,13 @@ class LikesController < ApplicationController
   def index
     article_id = params[:article_id]
     @article = Article.find article_id
-    like = Like.find_or_initialize_by(article_id: article_id, user_id: current_user.id)
-    if like.new_record?
-      like.save
-    else
-      like.destroy
+    if current_user
+      like = Like.find_or_initialize_by(article_id: article_id, user_id: current_user.id)
+      if like.new_record?
+        like.save
+      else
+        like.destroy
+      end
     end
   end
 
